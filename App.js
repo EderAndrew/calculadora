9b0e570f15dfb9d520b8fd42fc22608c2da99803
Ed_Andrew <ederandrew0028@gmail.com>
2018-09-02 23:07:13 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+
+class Botao extends Component{
+  constructor(props){
+    super(props)
+    this.state = {}
+
+    this.styles = StyleSheet.create({
+      area:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        borderWidth: 1,
+        borderColor:'#999999',
+        backgroundColor:'#E0E0E0'
+      },
+      text:{
+        fontSize:18
+      }
+    })
+  }
+
+  render(){
+    return(
+      <TouchableOpacity style={this.styles.area}>
+        <Text style={this.styles.text}></Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+export default class Calculadora extends Component{
+  render(){
+    return(
+      <View style={ StyleSheet.body }>
+        
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  body:{
+    flex: 1
+  }
+})
---
+++ README.md
@@ -0,0 +1,5 @@
+Projeto básico de React-native
+
+Versão 1.0
+
+app básico de uma calculadora. Apenas estudo.
